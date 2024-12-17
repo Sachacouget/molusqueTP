cea5f27ac66c8ccd181b1cb2928e88f1f435a4a0
Sachacouget <sacha.couget@viacesi.fr>
2024-12-17 14:01:59 +0100

    change flask network

--- molusque.py
+++ molusque.py
@@ -64,5 +64,9 @@ scheduler = BackgroundScheduler()
 scheduler.add_job(scheduled_task, 'interval', minutes=60)
 scheduler.start()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
